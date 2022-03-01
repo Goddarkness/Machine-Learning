3ec79dc4ac67e41515f29df3105f2994f8966d4d
Goddarkness <87402585+Goddarkness@users.noreply.github.com>
2022-03-01 21:50:21 +0800

    add some new function to perfect it

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,8 +1,35 @@
 #include "Matrix.h"
+
+void Matrix::print() {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cout << ma[i * n + j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
 void Matrix::setsize( int m, int n) {
     ma.resize(m*n);
     this->m = m;
     this->n = n;
+}
+
+void Matrix::changerow(int a, int b) {
+    std::vector<double>temp;
+    a = a - 1;
+    b = b - 1;
+    temp.resize(n);
+    for (int i = 0; i < n; i++) {
+        temp[i] = ma[a * n + i];
+    }
+    for (int i = 0; i < n; i++) {
+        ma[a*n+i] = ma[b * n + i];
+    }
+    for (int i = 0; i < n; i++) {
+        ma[b * n + i] = temp[i];
+    }
+
 }
 
 Matrix Matrix::operator*(Matrix A)
@@ -39,24 +66,25 @@ Matrix Matrix::transpose() {
 Matrix Matrix::operator+(Matrix A) {
     Matrix B;
     B.setsize( m, n);
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++)
-            B.ma[i * n + j] = ma[i * n + j] + A.ma[i * n + j];
+    for (int i = 0; i < m*n; i++) {
+        B.ma[i] = ma[i] + A.ma[i];
     }
+
+       
     return B;
-}
+}      //  +只是对应位置的相加，不需要用二维方式表示
 
 Matrix Matrix::operator-(Matrix A) {
     Matrix B;
     B.setsize( m, n);
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++)
-            B.ma[i * n + j] = ma[i * n + j] - A.ma[i * n + j];
+    for (int i = 0; i < m * n; i++) {
+        B.ma[i] = ma[i] + A.ma[i];
     }
     return B;
-}
+}  
+// -只是对应位置的相减，不需要二维方式的表示
 
-Matrix  Matrix::operator*(int a) {
+Matrix  Matrix::operator*(double a) {
     Matrix B;
     for (int i = 0; i < m * n; i++) {
         B.ma[i] = ma[i] * a;
@@ -64,7 +92,7 @@ Matrix  Matrix::operator*(int a) {
     return B;
 }
 
-Matrix Matrix::operator/(int a) {
+Matrix Matrix::operator/(double a) {
     Matrix B;
     for (int i = 0; i < m * n; i++) {
         B.ma[i] = ma[i] / a;
@@ -83,6 +111,8 @@ double Matrix::To_double() {
 }
 
 Matrix Matrix::subMatrrix(int row, int column) {
+    row = row - 1;
+    column = column - 1;
     Matrix B;
     B.setsize(m - 1, n - 1);
     int count = 0;
@@ -110,10 +140,140 @@ double Matrix::det() {
         double value = 0;
         int sign = 1;
         for (int i = 0; i < n; i++) {
-            value = value + sign * ma[i] * subMatrrix(0, i).det();
+            value = value + sign * ma[i] * subMatrrix(1, 1+i).det();
             sign = -sign;
         }
         return value;
     }
+
+}
+
+int Matrix::findMaxelememtline() {
+    std::vector <double> Max;
+    Max.resize(m);
+    for (int i = 0; i < m; i++) {
+        double max = ma[i * n];
+        for (int j = 1; j < n; j++) {
+            if (ma[i * n + j] > max)
+                max = ma[i * n + j];
+        }
+        Max[i] = max;
+    }
+    double max = Max[0];
+    int locate = 0;
+    for (int i = 1; i < m; i++) {
+        if (max < Max[i]) {
+            max = Max[i];
+            locate = i;
+        }
+           
+    }
+    return locate+1;
+}
+
+bool Matrix::closeenough( Matrix B) {
+    if (m != B.m  || n != B.n)
+        return false;
+    for (int i = 0; i < m * n; i++) {
+        if (abs(ma[i] - B.ma[i]) > 1.e-12) {
+            return false;
+            break;
+        }
+    }
+    return true;
+ }
+
+bool Matrix::operator==(Matrix B) {
+    if (closeenough(B))
+        return true;
+    else
+        return false;
+}
+
+Matrix Matrix::together() {
+    if (m != n) {
+        std::cout << " fail to combine Matrix" << std::endl;
+        return Matrix();
+    }
+    Matrix B;
+    B.setsize(m, 2 * n);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            B.ma[i * 2 * n + j] = ma[i * n + j];
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        B.ma[i * 2 * n + m + i] = 1;
+    }
+    return B;
+}
+
+Matrix Matrix::seperate() {
+    if (n != 2 * m) {
+        std::cout << "fail to separate this Matrix" << std::endl;
+        return Matrix();
+    }
+    Matrix B;
+    B.setsize(m, m);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            B.ma[i * m + j] = ma[i *  n + j+m];
+        }
+    }
+    return B;
+}
+
+Matrix Matrix::mutiply(double a, int row) {
+    Matrix B;
+    B.setsize(m, n);
+    for (int i = 0; i < m * n; i++) {
+        B.ma[i] = ma[i];
+    }
+    for (int i = 0; i < n; i++) {
+        B.ma[(row - 1) * n + i] = ma[(row - 1) * n + i] * a;
+    }
+    return B;
+}
+
+Matrix Matrix::mutiply(double a, int row_1, int row_2) {
+    Matrix B;
+    B.setsize(m, n);
+    for (int i = 0; i < m * n; i++) {
+        B.ma[i] = ma[i];
+    }
+    for (int i = 0; i < n; i++) {
+        B.ma[(row_2 - 1) * n + i] = B.ma[(row_2 - 1) * n + i] + ma[(row_1 - 1) * n + i] * a;
+    }
+    return B;
+}
+
+bool Matrix::IsIdentifyMatrix() {
+    if (m != n) {
+        return false;
+    }
+    Matrix B;
+    B.setsize(m, n);
+    for (int i = 0; i < m; i++) {
+        B.ma[i * n + i] = 1;
+    }
+    for (int i = 0; i < m * n; i++) {
+        if (ma[i] != B.ma[i]) {
+            return false;
+            break;
+        }
+    }
+    return true;
+}
+
+Matrix Matrix::inverse() {
+    if (m != n) {
+        std::cout << "矩阵不是方阵，无法求逆矩阵" << std::endl;
+        return Matrix();
+    }
+    Matrix B;
+    B=together();
+    B.print();
+    return Matrix();
+
 
 }
--- Matrix.h
+++ Matrix.h
@@ -5,17 +5,35 @@
 
 class Matrix {
 public:
+    std::vector <double> ma;  // 矩阵
+    int m = 0, n = 0; // 行 ，列
+
+    double To_double(); // 将一维矩阵变成double类型实数
     void setsize( int m, int n); //设计矩阵规模
+    void changerow(int a, int b ); // 换行
+
     Matrix transpose();  // 矩阵转置
     Matrix operator*(Matrix A);
     Matrix operator+(Matrix A);
     Matrix operator-(Matrix A);
-    Matrix operator*(int a);
-    Matrix operator/(int a);
-    Matrix subMatrrix(int row, int column); // 找次级矩阵
-    double To_double();
-    double det(); //求行列式
-    std::vector <double> ma;  // 矩阵
-    int m = 0, n = 0; // 行 ，列
+    Matrix operator*(double a);
+    Matrix operator/(double a);
+    bool operator==(Matrix B);// 判断两矩阵是否相等
+
+    Matrix subMatrrix(int row, int column); // 找次级矩阵，用于服务det函数
+    double det(); //求行列式 
+
+    
+    Matrix together(); // 合并两个矩阵，用于服务inverse函数
+    Matrix seperate(); // 分开两个矩阵，用于服务inverse函数
+    Matrix mutiply(double a, int row); // 对矩阵column乘a
+    Matrix mutiply(double a, int row_1, int row_2);
+    bool IsIdentifyMatrix();// 判断是否为单位矩阵
+    Matrix inverse();//求逆矩阵
+
+
+    void print(); // 输出矩阵
+    int findMaxelememtline(); // 找到拥有最大元素所在的行
+    bool closeenough(Matrix B); // 用于服务 == ， 两矩阵在误差范围内则返回true
 };
 #endif // !MATRIX
